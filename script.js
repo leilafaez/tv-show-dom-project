cc311365318914febdfc646baa5303993a362e6b
leilafaez <leila.faez@gmail.com>
2022-11-01 16:23:14 +0000

    Update html-css and js file
    add header
    add search textbox
    make main section for every episod list item
    style header and main

--- script.js
+++ script.js
@@ -2,11 +2,35 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  console.log(allEpisodes);
 }
+
+
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach(episod=>{
+
+    const episodContainer= document.createElement("div");
+    const title=document.createElement("h3");
+    const image=document.createElement("img");
+    const summary=document.createElement("p");
+        
+    title.textContent=`${episod.name}`
+    image.src=`${episod.image.medium}`
+    summary.textContent=`${episod.summary}`.replace("<p>","").replace("</p>","");
+
+    episodContainer.appendChild(title);
+    episodContainer.appendChild(image);
+    episodContainer.appendChild(summary);
+    rootElem.appendChild(episodContainer);
+
+    
+  })
 }
 
 window.onload = setup;
+
+
+
