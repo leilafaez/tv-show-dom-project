2bfea71dd29d9d7629f476dfa5f5acc32add72c8
leilafaez <leila.faez@gmail.com>
2022-11-26 21:50:05 +0000

    fix some bugs

--- finalApiScript.js
+++ finalApiScript.js
@@ -1,4 +1,3 @@
-
 const rootElem = document.getElementById("root");
 const form = document.getElementById("form");
 const searchInput = document.getElementById("search");
@@ -9,49 +8,20 @@ const showName = getAllShows();
 showNameList(showName);
 let shows;
 
-let endpointEpisod = "https://api.tvmaze.com/shows/82/episodes";
-fetch(endpointEpisod)
-  .then((Response) => {
-    return Response.json();
-  })
-  .then((data) => {
-    makePageForEpisodes(data);
-    search(data);
-    selectEpisod(data);
-
-    console.log(data);
-  });
-// function getEpisodWithAPI(endpointEpisod = "https://api.tvmaze.com/shows/82/episodes"){
-//     // rootElem.innerHTML="";
-//     const response=fetch(endpointEpisod);
-//     const data= response.json();
-//     search(data);
-//     // selectEpisod(data);
-// }
-
-// function getShowWithAPI(endpointShow = "https://api.tvmaze.com/shows"){
-//     rootElem.innerHTML="";
-//     const response=fetch(endpointShow);
-//     const data=response.json();
-//     makePageForShows(data);
-//     showNameList(data);
-//     searchForShow(data);
-// }
-
 // make page by shows
 function makePageForShows(showList) {
   showList.forEach((show) => {
     const showContainer = document.createElement("section");
-    showContainer.setAttribute("id","show-section");
+    showContainer.setAttribute("id", "show-section");
     const title = document.createElement("h3");
     const image = document.createElement("img");
     const summary = document.createElement("p");
 
     title.textContent = show.name;
-    if(show.image && show.image.medium){
-        image.src = `${show.image.medium}`;
+    if (show.image && show.image.medium) {
+      image.src = `${show.image.medium}`;
     }
-    
+
     summary.textContent = `${show.summary}`.replace(/(<([^>]+)>)/gi, "");
 
     showContainer.appendChild(title);
@@ -78,40 +48,11 @@ function makePageForShows(showList) {
     showDetailItem.appendChild(showDetailRuntime);
 
     rootElem.appendChild(showContainer);
-    showContainer.addEventListener("click",()=>{
-    //  debugger
+    showContainer.addEventListener("click", () => {
+      //  debugger
       getEpisod(show.id);
-      
     });
   });
-}
-
-// function loadEpisodByShowClick(){
-//     let sectionOfShow = document.getElementById("show-section");
-//     sectionOfShow.addEventListener("click", getEpisod);
-// }
-
-//make a main page by episodes
-function makePageForEpisodes(episodeList) {
- 
-  episodeList.forEach((episod) => {
-    const episodContainer = document.createElement("div");
-    const title = document.createElement("h3");
-    const image = document.createElement("img");
-    const summary = document.createElement("p");
-
-    episod.number < 10 && episod.season < 10
-      ? (title.textContent = `${episod.name}-S0${episod.number}E0${episod.season}`)
-      : (title.textContent = `${episod.name}-S${episod.number}E${episod.season}`);
-    image.src = `${episod.image.medium}`;
-    summary.textContent = `${episod.summary}`.replace(/(<([^>]+)>)/gi, "");
-
-    episodContainer.appendChild(title);
-    episodContainer.appendChild(image);
-    episodContainer.appendChild(summary);
-    rootElem.appendChild(episodContainer);
-  });
-
 }
 
 //function for creating each episode card
@@ -131,25 +72,24 @@ function episodeCards(episodeList) {
 }
 
 //function for creating each show card
-function showCards(showList){
-    let html="";
-    showList.forEach((show)=>{
-        if(show.image && show.image.medium){
-          html += `<section>
+function showCards(showList) {
+  let html = "";
+  showList.forEach((show) => {
+    if (show.image && show.image.medium) {
+      html += `<section>
         <h3>${show.name}</h3>
         <img src=${show.image.medium}>
         <p>${show.summary}</p>
         </section>`;
-        }
-    })
-    rootElem.innerHTML=html;
+    }
+  });
+  rootElem.innerHTML = html;
 }
 //  search section for episod
 
 function search(episodlist) {
   searchInput.addEventListener("keyup", (e) => {
     let value = e.target.value;
-
     let search = episodlist.filter(
       (element) =>
         element.name.toLowerCase().includes(value) ||
@@ -163,35 +103,34 @@ function search(episodlist) {
   });
 }
 //  search section for show
-function searchForShow(showList){
-    searchInput.addEventListener("keyup",(e)=>{
-        let value = e.target.value;
-        let searchShow=showList.filter((show)=>{
-            if(show.genres){
-                const genres=show.genres.join(" ").toLowerCase();
-                return(
-                    show.name.toLowerCase().includes(value) ||
-                    show.summary.toLowerCase().includes(value) ||
-                    genres.includes(value)
-                );
-            }else{
-                return (
-                  show.name.toLowerCase().includes(value) ||
-                  show.summary.toLowerCase().includes(value)
-                );
-            }
-        });
-        const searchShowLength= searchShow.length;
-        const totalShows=showList.length;
-        count.textContent = `Search Count :${searchShowLength}/${totalShows}`;
-        showCards(searchShow);
-    })
+function searchForShow(showList) {
+  searchInput.addEventListener("keyup", (e) => {
+    let value = e.target.value;
+    let searchShow = showList.filter((show) => {
+      if (show.genres) {
+        const genres = show.genres.join(" ").toLowerCase();
+        return (
+          show.name.toLowerCase().includes(value) ||
+          show.summary.toLowerCase().includes(value) ||
+          genres.includes(value)
+        );
+      } else {
+        return (
+          show.name.toLowerCase().includes(value) ||
+          show.summary.toLowerCase().includes(value)
+        );
+      }
+    });
+    const searchShowLength = searchShow.length;
+    const totalShows = showList.length;
+    count.textContent = `Search Count :${searchShowLength}/${totalShows}`;
+    showCards(searchShow);
+  });
 }
 
 //dropdown list for episods
 function selectEpisod(searchEpisod) {
-
-  searchEpisod.forEach((episod) => {
+    searchEpisod.forEach((episod) => {
     const option = document.createElement("option");
 
     option.setAttribute("value", episod.name);
@@ -239,12 +178,6 @@ function showNameList(ListOfShowsName) {
     listOfShows.appendChild(optionShow);
   });
 }
-//search for show dropdown
-// function getSelectedURL() {
-//   const selectedOption = listOfShows.value;
-
-//   return `https://api.tvmaze.com/shows/${selectedOption}/episodes`;
-// }
 
 function getEpisod(episodID) {
   console.log(episodID);
@@ -254,15 +187,16 @@ function getEpisod(episodID) {
       return response.json();
     })
     .then((data) => {
+      selectEpisod(data)
       // console.log(data);
-      unHideEpisodeSelect();
+      showEpisodeSelect();
       let html = "";
       data.forEach((show) => {
-       const doubleShowNumber = show.number < 10 ? "0" : "";
-      html += `<div>
+        const doubleShowNumber = show.number < 10 ? "0" : "";
+        html += `<div>
       <h3>${show.name}-S${doubleShowNumber}${show.number}E${
-      show.season < 10 ? "0" : ""
-      }${show.season}</h3>
+          show.season < 10 ? "0" : ""
+        }${show.season}</h3>
       <img src=${show.image.medium}>
       <p>${show.summary}</p>
       </div>`;
@@ -270,28 +204,27 @@ function getEpisod(episodID) {
       rootElem.innerHTML = html;
     });
 }
-listOfShows.addEventListener("change",()=>{
+listOfShows.addEventListener("change", () => {
   console.log(listOfShows);
-   getEpisod(listOfShows.value);
+  getEpisod(listOfShows.value);
 });
 
-function hideEpisodeSelect(){
-    select.style.display="none";
+function hideEpisodeSelect() {
+  select.style.display = "none";
 }
 
-function unHideEpisodeSelect(){
-    select.style.display="block";
+function showEpisodeSelect() {
+  select.style.display = "block";
 }
 
 function setup() {
-shows=getAllShows();
+  shows = getAllShows();
   makePageForShows(shows);
   showNameList(shows);
-searchForShow(shows);
-hideEpisodeSelect();
- loadEpisodByShowClick();
-//   searchInput.addEventListener("keyup", searchForShow(shows));
-  
+  searchForShow(shows);
+  hideEpisodeSelect();
+  //  loadEpisodByShowClick();
+  //   searchInput.addEventListener("keyup", searchForShow(shows));
 }
 
 window.onload = setup;
